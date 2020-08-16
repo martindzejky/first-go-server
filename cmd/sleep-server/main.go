c9036449b2019c37ee38a572ce7838cd98bbb45a
Martin Jakubik <martin@jakubik.info>
2020-08-16 18:44:22 +0200

    use mux in sleep server

--- cmd/sleep-server/main.go
+++ cmd/sleep-server/main.go
@@ -1,28 +1,65 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"log"
 	"math/rand"
 	"net/http"
+	"os"
+	"os/signal"
 	"time"
 
+	"github.com/gorilla/mux"
 	"github.com/martindzejky/first-go-server/internal/http-utils"
 	"github.com/martindzejky/first-go-server/internal/responses"
 )
 
 func main() {
 	port := "8080"
-	route := "/api/sleep"
+	router := mux.NewRouter()
+
+	log.Println("Creating the server")
+	server := &http.Server{
+		Addr:         "0.0.0.0:" + port,
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
+		Handler:      router,
+	}
 
 	log.Println("Seeding the random number generator")
 	rand.Seed(time.Now().UnixNano())
 
-	log.Println("Registering the \"" + route + "\" handler")
-	http.HandleFunc(route, sleepHandler)
+	log.Println("Registering request handlers")
+	router.HandleFunc("/api/sleep", sleepHandler)
+	http.Handle("/", router)
 
-	log.Println("Server listening on port", port)
-	log.Fatalln(http.ListenAndServe(":"+port, nil))
+	go func() {
+		log.Println("Starting the server on port", port)
+		err := server.ListenAndServe()
+
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt)
+
+	// block until the signal is received
+	<-signals
+
+	// make a context to wait for connections
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	log.Println("Gracefully shutting down the server")
+	err := server.Shutdown(ctx)
+
+	if err != nil {
+		log.Fatalln("Error while shutting down the server:", err)
+	}
 }
 
 func sleepHandler(res http.ResponseWriter, req *http.Request) {
@@ -30,22 +67,29 @@ func sleepHandler(res http.ResponseWriter, req *http.Request) {
 
 	// there's a small chance that the request fails
 	if rand.Float32() < 0.05 {
-		http.Error(res, "Simulated request failure", http.StatusBadRequest)
+		http.Error(res, "Simulated request failure", http.StatusInternalServerError)
 		return
 	}
 
+	// get the sleeping time
 	query := req.URL.Query()
 	min := httpUtils.GetQueryIntValue(query, "min", 100)
 	max := httpUtils.GetQueryIntValue(query, "max", 300)
 
-	waitTime := rand.Intn(max-min) + min
+	sleepTime := rand.Intn(max-min) + min
 
-	log.Println("Sleeping for", waitTime, "ms")
-	time.Sleep(time.Duration(waitTime) * time.Millisecond)
+	log.Println("Sleeping for", sleepTime, "ms")
+
+	select {
+	case <-req.Context().Done():
+		log.Println("Request canceled:", req.Context().Err())
+		return
+	case <-time.After(time.Duration(sleepTime) * time.Millisecond):
+	}
 
 	res.Header().Set("Content-Type", "application/json")
 	err := json.NewEncoder(res).Encode(responses.SleepResponse{
-		Time: waitTime,
+		Time: sleepTime,
 	})
 
 	if err != nil {
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/martindzejky/first-go-server
 
 go 1.14
+
+require github.com/gorilla/mux v1.7.4
